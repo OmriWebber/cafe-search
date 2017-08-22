6ab8e5c245ccf072d0b1ceb213929cb41c63df4c
Omri Webber <omriwebber@MacbookPro.local>
2017-08-23 10:53:17 +1200

    blah

--- public/js/script.js
+++ public/js/script.js
@@ -18,7 +18,7 @@ function getAccessToken(data){
 
 function getData(accessToken){
   $.ajax({
-    url: "https://graph.facebook.com/v2.10/" + data.name + "?fields=about%2Clocation&access_token=EAABy0zkU5Y0BAK7S9BVN4Suj95GMJWgrK8lZA9yBpkrGfVPE7fAgKB9uBYS3NH52ekJVEGgHkAgj4uRQ0rn5Wwh7RuiuCigc7KQojn2JSiofxZCN9aJikK16L5B91P3MU9BxbdzDZCDZA1JhUEIdAhlZAdhDdAxulxRvbOiS3ymAMy2ZBvHIpa3ExU2F1oOkWJIFf7koADtwZDZD",
+    url: "https://graph.facebook.com/v2.10/" + data.name + "?fields=about%2Clocation&access_token="+accessToken,
     dataType: "jsonp",
     success: function(dataFromFacebook){
       console.log(dataFromFacebook);

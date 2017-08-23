ce77e69d6b245f48d0a6c88c344a49c13795569f
Omri Webber <omriwebber@MacbookPro.local>
2017-08-23 13:24:14 +1200

    Working on getting facebook data

--- public/js/script.js
+++ public/js/script.js
@@ -1,46 +1,69 @@
-// var accessToken;
+var accessToken, fbPageID;
 
-$.ajax({
-  url: "/data/data.json",
-  dataType: "json",
-  success: function(data){
-    console.log(data.places);
-    for (var i = 0; i < data.places.length; i++) {
-      $("#placesList").append("<li class='place' onclick='showInfo();'>" + data.places[i].name + "</li>");
-    }
-  },
-  error: function(){
-    console.log("something went wrong.");
-  }
-});
+$(document).ready(function(){
 
-function showInfo(){
-  console.log($(this));
+  $("#backToList").click(function(){
+    $('#sidebar').removeClass('slideOutLeft').addClass('slideInLeft');
+    $('#placeInfo').removeClass('slideInRight').addClass('slideOutRight');
+  });
+
   $.ajax({
-    url: "./config.json",
+    url: "/data/data.json",
     dataType: "json",
-    success: function(configData){
-      accessToken = configData.fAccessToken;
-      getData(accessToken);
+    success: function(data){
+      console.log(data.places);
+      for (var i = 0; i < data.places.length; i++) {
+        $("#placesList").append("<li class='place'>" + data.places[i].name + "</li>");
+      }
+      $('.place').click(function() {
+          var places = this.innerText;
+          for (var i = 0; i < data.places.length; i++) {
+            if(places == data.places[i].name) {
+              fbPageID = data.places[i].fbID;
+              $('#sidebar').removeClass('slideInLeft').addClass('slideOutLeft');
+              $('#placeInfo').removeClass('slideOutRight').addClass('slideInRight');
+              if(fbPageID == "null") {
+                $("#error").text("Sorry, " + data.places[i].name + " does not have a facebook page");
+                return;
+              }
+              $("#error").empty();
+              showInfo();
+              return;
+            }
+          }
+      });
     },
     error: function(){
-      console.log("Couldn't get Access Token");
-    }
-  });
-}
-
-function getData(accessToken){
-  $.ajax({
-    url: "https://graph.facebook.com/v2.10/OliveCafe?fields=about%2Clocation&access_token="+accessToken,
-    dataType: "jsonp",
-    success: function(dataFromFacebook){
-      console.log(dataFromFacebook);
-    },
-    error: function(){
-      console.log("Couldn't get Facebook data");
+      console.log("something went wrong.");
     }
   });
 
-  $('#placesList').addClass('slideOutLeft');
-  $('#placeInfo').show().addClass('slideInRight');
-}
+
+  function showInfo(){
+    $.ajax({
+      url: "./config.json",
+      dataType: "json",
+      success: function(configData){
+        accessToken = configData.fAccessToken;
+        getData(accessToken);
+      },
+      error: function(){
+        console.log("Couldn't get Access Token");
+      }
+    });
+  }
+
+  function getData(accessToken){
+    $.ajax({
+      url: "https://graph.facebook.com/v2.10/" + fbPageID + "?fields=about%2Clocation&access_token="+accessToken,
+      dataType: "jsonp",
+      success: function(dataFromFacebook){
+        console.log(dataFromFacebook);
+      },
+      error: function(){
+        console.log("Couldn't get Facebook data");
+      }
+    });
+  }
+
+});
